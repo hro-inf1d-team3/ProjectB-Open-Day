b1087bbf7a8d4b9edd3e5109ec9eb469f9bc8bc6
mosaadaldeen <Mohammadsd97@gmail.com>
2019-04-02 11:47:48 +0200

    added a new button about information the institute (Hogeschool Rotterdam)

---
+++ app/src/main/java/app/inf1d_team3/open_day/InstituteInfoActivity.java
@@ -0,0 +1,13 @@
+package app.inf1d_team3.open_day;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstituteInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_institute_info);
+    }
+}
--- app/src/main/java/app/inf1d_team3/open_day/MainActivity.java
+++ app/src/main/java/app/inf1d_team3/open_day/MainActivity.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
         LocalDatabase.init();
     }
 
+    // dat is om van een pagina naar andere te gaan
     public void openOpenDaysList(View view) {
         Intent intent = new Intent(this, OpenDaysActivity.class);
         startActivity(intent);
@@ -24,5 +35,11 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         LocalDatabase.destroy();
+    }
+
+    //dat is de rest van de boveaan code
+    public void openActivity2(){
+        Intent intent = new Intent(MainActivity.this, InstituteInfoActivity.class);
+        startActivity(intent);
     }
 }
