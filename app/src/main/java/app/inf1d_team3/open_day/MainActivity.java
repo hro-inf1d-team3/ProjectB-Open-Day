61fdbd67a12deddfd0e0a68e22be94a1e2b44b2e
Matthijs Booman <matthijs.booman@hotmail.com>
2019-05-21 17:01:27 +0200

    added new main activity & bottom navigation

---
+++ app/src/main/java/app/inf1d_team3/open_day/MainActivity.java
@@ -0,0 +1,39 @@
+package app.inf1d_team3.open_day;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_openDays:
+                    mTextMessage.setText(R.string.title_openDays);
+                    return true;
+                case R.id.navigation_askQuestion:
+                    mTextMessage.setText(R.string.title_askQuestion);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
