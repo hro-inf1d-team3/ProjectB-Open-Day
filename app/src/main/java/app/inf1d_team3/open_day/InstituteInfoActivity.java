8e3a1df46925e693cde9b92e5786f937461c12d6
mosaadaldeen <Mohammadsd97@gmail.com>
2019-05-27 15:43:54 +0200

    All the work was in the activity_institute_info.xml activity: Added a photo to the info page. Added the location(text). Added a button that takes the users to the website of the hogeschool Rotterdam to the CMI studies.

--- app/src/main/java/app/inf1d_team3/open_day/InstituteInfoActivity.java
+++ app/src/main/java/app/inf1d_team3/open_day/InstituteInfoActivity.java
@@ -1,7 +1,10 @@
 package app.inf1d_team3.open_day;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InstituteInfoActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class InstituteInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_institute_info);
+    }
+
+    public void open(View view) {
+        Intent browaerIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.hogeschoolrotterdam.nl/opleidingen/?areas=mediait"));
+        startActivity(browaerIntent);
+
     }
 }
